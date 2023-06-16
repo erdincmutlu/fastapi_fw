abc5a677fa6b0565c126f3a370045f3e902642d9
Erdinc Mutlu <erdinc@deeptime.io>
2023-06-16 16:16:57 +0100

    Path params

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-     return {'data':{'name': 'Erdinc'}}
+     return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-     return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+     return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+     # fetch blog with id = id
+     return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+     # fetch comments of blog id = id
+     return {'data': {"1", "2"}}
