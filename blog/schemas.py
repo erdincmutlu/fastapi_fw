80b9a80cbffe1fde8fd1636f6055359aeae754c9
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-06-19 20:59:24 +0100

    Create user

--- blog/schemas.py
+++ blog/schemas.py
@@ -6,5 +6,12 @@ class Blog(BaseModel):
 
 class ShowBlog(BaseModel):
     title: str
+    body: str
+
     class Config():
         orm_mode = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
